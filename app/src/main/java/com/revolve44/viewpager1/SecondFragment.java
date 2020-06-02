348377c7327fd78259a10dd754fadca24e55473d
ArsMars <tag_mahal@mail.ru>
2020-06-02 10:19:53 +0300

    first commit :rocket:

---
+++ app/src/main/java/com/revolve44/viewpager1/SecondFragment.java
@@ -0,0 +1,43 @@
+package com.revolve44.viewpager1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+    // Store instance variables
+    private String title;
+    private int page;
+
+    // newInstance constructor for creating fragment with arguments
+    public static FirstFragment newInstance(int page, String title) {
+        FirstFragment fragmentFirst = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt2", page);
+        args.putString("someTitle2", title);
+        fragmentFirst.setArguments(args);
+        return fragmentFirst;
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt2", 0);
+        title = getArguments().getString("someTitle2");
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel2);
+        tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
